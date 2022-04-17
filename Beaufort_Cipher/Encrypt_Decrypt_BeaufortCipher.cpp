76abc9c1aeb79fa77595e76d15d9ace671f1b203
selvijebucolli1 <selvije.bucolli@student.uni-pr.edu>
2022-04-17 19:21:46 +0200

    Input,Output

--- Beaufort_Cipher/Encrypt_Decrypt_BeaufortCipher.cpp
+++ Beaufort_Cipher/Encrypt_Decrypt_BeaufortCipher.cpp
@@ -30,5 +30,36 @@ string beaufort(string text, string key) {
 
 
 int main() {
+    string text, key, out;
+    int komanda;
+
+    cout << "Mire erdhet ne programin per enkriptim dhe dekriptim te tekstit permes Beaufort Cipher!" << endl;
+    cout << "Meny: " << endl;
+    cout << "    --Shtypni 1 per enkriptim " << endl;
+    cout << "    --Shtypni 2 per dekriptim " << endl;
+    cin >> komanda;
+    
+
+    switch (komanda)
+    {
+    case 1:
+        cout << "Jepni plaintext-in : ";
+        cin >> text;
+        cout << "Jepni celesin per enkriptim : ";
+        cin >> key;
+        out = beaufort(text, key);
+        cout << "Ciphertext-i : " << out << "\n"; break;
+    case 2: 
+        cout << "Jepni Ciphertext-in : ";
+        cin >> text;
+        cout << "Jepni Celesin per dekriptim: ";
+        cin >> key;
+        out = beaufort(text, key);
+        cout << "Plaintext-i: " << out << "\n"; break;
+
+    default:
+        cout << "Komand e gabuar!";
+        break;
+    }
     
 }
