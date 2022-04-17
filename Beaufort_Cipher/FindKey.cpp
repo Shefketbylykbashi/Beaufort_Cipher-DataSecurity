d8373959c851a48cca7cd63e18bafe50b8871f7d
Sueda Guri <suedagurii@gmail.com>
2022-04-17 19:44:10 +0200

    Findkey File

--- Beaufort_Cipher/FindKey.cpp
+++ Beaufort_Cipher/FindKey.cpp
@@ -4,9 +4,22 @@ using namespace std;
 
 class Key {
 
-    
+  string findKey(string t) {
+        string output;
+        for (int i = 0, j = 0; i < t.length(); ++i) {
+            char c = t[i];
+            if (c >= 'a' && c <= 'z')
+                c += 'A' - 'a';
+            else if (c < 'A' || c > 'Z')
+                continue;
+            output += (c + k[j] - 2 * 'A') % 26 + 'A'; //added 'A' to bring it in range of ASCII alphabet [ 65-90 | A-Z ]
+            j = (j + 1) % k.length();
+        }
+        return output;
+    }  
 };
 int main() {
     
-    
+string findKey = v.findKey(plain);
+cout << "Celesi: " << findKey << endl;
 }
